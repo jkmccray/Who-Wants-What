fb16acfdcc80af21749443dfbf845920d681cd10
Jacquelyn M <jacquelynkmccray@gmail.com>
2019-12-09 22:26:22 -0600

    Added styling for viewing groups in grid format. Added functionality for creating and leaving groups. Working on search for people to add to groups

--- WhoWantsWhat/Controllers/GroupsController.cs
+++ WhoWantsWhat/Controllers/GroupsController.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using WhoWantsWhat.Data;
 using WhoWantsWhat.Models;
+using WhoWantsWhat.Models.ViewModels.GroupsViewModels;
 
 namespace WhoWantsWhat.Controllers
 {
@@ -27,9 +28,12 @@ namespace WhoWantsWhat.Controllers
         public async Task<IActionResult> Index()
         {
             var user = await GetCurrentUserAsync();
-            return View(await _context.Groups
-                .Where(g => g.GroupUsers.Any(gu => gu.User == user))
-                .ToListAsync());
+            var groups = await _context.Groups
+                .Include(g => g.GroupUsers)
+                .ThenInclude(gu => gu.User)
+                .Where(g => g.GroupUsers.Any(gu => gu.User == user && gu.Joined))
+                .ToListAsync();
+            return View(groups);
         }
 
         // GET: Groups/Details/5
@@ -40,16 +44,16 @@ namespace WhoWantsWhat.Controllers
                 return NotFound();
             }
 
-            var group = await _context.Groups
+            var @group = await _context.Groups
                 .Include(g => g.GroupUsers)
                 .ThenInclude(gu => gu.User)
                 .FirstOrDefaultAsync(m => m.GroupId == id);
-            if (group == null)
+            if (@group == null)
             {
                 return NotFound();
             }
 
-            return View(group);
+            return View(@group);
         }
 
         // GET: Groups/Create
@@ -69,57 +73,17 @@ namespace WhoWantsWhat.Controllers
             {
                 _context.Add(@group);
                 await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
-            }
-            return View(@group);
-        }
 
-        // GET: Groups/Edit/5
-        public async Task<IActionResult> Edit(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var @group = await _context.Groups.FindAsync(id);
-            if (@group == null)
-            {
-                return NotFound();
-            }
-            return View(@group);
-        }
-
-        // POST: Groups/Edit/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("GroupId,Name")] Group @group)
-        {
-            if (id != @group.GroupId)
-            {
-                return NotFound();
-            }
-
-            if (ModelState.IsValid)
-            {
-                try
+                var groupId = @group.GroupId;
+                var user = await GetCurrentUserAsync();
+                _context.Add(new GroupUser
                 {
-                    _context.Update(@group);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!GroupExists(@group.GroupId))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
+                    GroupId = groupId,
+                    UserId = user.Id,
+                    Joined = true
+                });
+                await _context.SaveChangesAsync();
+
                 return RedirectToAction(nameof(Index));
             }
             return View(@group);
@@ -153,6 +117,62 @@ namespace WhoWantsWhat.Controllers
             await _context.SaveChangesAsync();
             return RedirectToAction(nameof(Index));
         }
+
+        // GET: Leave group
+        public async Task<IActionResult> LeaveGroup(int? GroupId)
+        {
+            if (GroupId == null)
+            {
+                return NotFound();
+            }
+
+            var groupUser = await _context.GroupUsers
+                .Include(gu => gu.Group)
+                .FirstOrDefaultAsync(gu => gu.GroupId == GroupId);
+            if (groupUser == null)
+            {
+                return NotFound();
+            }
+
+            return View(groupUser);
+        }
+
+        // POST: Leave group confirmed
+        //[HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> LeaveGroupConfirmed(int GroupId)
+        {
+            var groupUser = await _context.GroupUsers.FirstOrDefaultAsync(gu => gu.GroupId == GroupId);
+            _context.GroupUsers.Remove(groupUser);
+            await _context.SaveChangesAsync();
+
+            var inactiveGroup = await _context.Groups.FirstOrDefaultAsync(g => g.GroupId == GroupId);
+            if (inactiveGroup.GroupUsers == null)
+            {
+                _context.Groups.Remove(inactiveGroup);
+                await _context.SaveChangesAsync();
+            }
+            return RedirectToAction(nameof(Index));
+        }
+
+        public async Task<IActionResult> AddPeopleToGroup(int GroupId)
+        {
+            var viewModel = new AddToPeopleToGroupViewModel
+            {
+                Group = await _context.Groups
+                    .Include(g => g.GroupUsers)
+                    .FirstOrDefaultAsync(g => g.GroupId == GroupId),
+                CurrentUser = await GetCurrentUserAsync()
+            };
+
+            return View(viewModel);
+        }
+
+        //[HttpPost, ActionName("AddPeopleToGroup")]
+        //public async Task<IActionResult> SearchForPeople(AddToPeopleToGroupViewModel viewModel)
+        //{
+
+        //}
 
         private bool GroupExists(int id)
         {
---
+++ WhoWantsWhat/Models/ViewModels/GroupsViewModels/AddToPeopleToGroupViewModel.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WhoWantsWhat.Models.ViewModels.GroupsViewModels
+{
+    public class AddToPeopleToGroupViewModel
+    {
+        public Group Group { get; set; }
+        public ApplicationUser CurrentUser { get; set; }
+        public string SearchText { get; set; }
+        public List<ApplicationUser> Users { get; set; } = new List<ApplicationUser>();
+    }
+}
