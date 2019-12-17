f67b699a993f69c1b24c1f11fae0ae8b18e5c5c5
Jacquelyn M <jacquelynkmccray@gmail.com>
2019-12-17 11:09:48 -0600

    added buttons to mark items as purchased and add purchased amount

---
+++ WhoWantsWhat/Models/ViewModels/ItemsViewModels/MarkItemAsPurchasedViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WhoWantsWhat.Models.ViewModels.ItemsViewModels
+{
+    public class MarkItemAsPurchasedViewModel
+    {
+        public int ItemId { get; set; }
+        public int GiftListId { get; set; }
+        public int WishListId { get; set; }
+    }
+}
