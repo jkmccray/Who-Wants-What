92a3a4a099309c584003834aabdf072ef33c0ca5
Jacquelyn M <jacquelynkmccray@gmail.com>
2020-01-12 23:21:33 -0600

    Added notifications for upcoming birthdays and due dates for gift lists within the next two months

---
+++ WhoWantsWhat/Models/ViewModels/HomeViewModels/NotificationsViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WhoWantsWhat.Models.ViewModels.HomeViewModels
+{
+    public class NotificationsViewModel
+    {
+        public List<ApplicationUser> UsersWithBirthdaysThisMonth { get; set; }
+        public List<ApplicationUser> UsersWithBirthdaysNextMonth { get; set; }
+        public List<GiftList> UpcomingGiftListsThisMonth { get; set; }
+        public List<GiftList> UpcomingGiftListsNextMonth { get; set; }
+    }
+}
